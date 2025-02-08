073886f9bf26e676628462e1cdbef42231ac8540
Teodor Todorov <teodortodorov752@gmail.com>
2025-02-08 17:15:18 +0200

    MVP functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,55 @@ import './App.css';
 function App() {
     const [timeLeft, setTimeLeft] = useState(25 * 60);
     const [isRunning, setIsRunning] = useState(false);
+    const [sessionType, setSessionType] = useState('work');
 
     useEffect(() => {
-        if (isRunning) {
+        if (isRunning && timeLeft > 0) {
             const countdownInterval = setInterval(() => {
-                setTimeLeft((prevTime) => {
-                    if (prevTime <= 1) {
-                        // Stop at 0
-                        setIsRunning(false);
-
-                        return 25 * 60;
-                    }
-                    return prevTime - 1;
-                });
+                setTimeLeft((prevTime) => prevTime - 1);
             }, 1000);
 
             return () => clearInterval(countdownInterval);
         }
-    }, [isRunning]);
+
+        if (timeLeft === 0) {
+            setIsRunning(false);
+            setSessionType((prevType) => {
+                setTimeLeft(prevType === 'work' ? 5 * 60 : 25 * 60);
+                return prevType === 'work' ? 'break' : 'work';
+            });
+        }
+    }, [isRunning, timeLeft]);
 
     const toggleTimer = () => {
         setIsRunning(!isRunning);
     };
 
+    const toggleSession = () => {
+        setSessionType((prevType) => {
+            setTimeLeft(prevType === 'work' ? 5 * 60 : 25 * 60);
+            return prevType === 'work' ? 'break' : 'work';
+        });
+        setIsRunning(false);
+    };
+
     return (
-        <div className="text-center text-3xl font-bold">
+        <div className="flex flex-col items-center text-3xl font-bold">
             <p>{`${
                 Math.floor(timeLeft / 60) > 9
                     ? Math.floor(timeLeft / 60)
                     : `0${Math.floor(timeLeft / 60)}`
             }:${timeLeft % 60 > 9 ? timeLeft % 60 : `0${timeLeft % 60}`}`}</p>
+            <div className='flex flex-col'>
             <button onClick={toggleTimer}>
                 {isRunning ? 'Stop' : 'Start'}
             </button>
+
+            <button onClick={toggleSession}>
+                {sessionType === 'work' ? 'Break' : 'Work'}
+            </button>
+            </div>
+            <p>Current Session: {sessionType.toUpperCase()}</p>
         </div>
     );
 }
